db2909af7518a2f04ca4cb27c4c69f59f4e58a7d
someotb <llyryk934@gmail.com>
2025-12-12 15:41:27 +0700

    feat: change path

--- src/dataparser.py
+++ src/dataparser.py
@@ -2,7 +2,7 @@ from random import randint
 
 
 def lines_parser():
-    with open("data.dat", "r", encoding="UTF-8") as file:
+    with open("../data/data.dat", "r", encoding="UTF-8") as file:
         rand_num = randint(0, 99)
         lines = file.readlines()
         return lines[rand_num].strip()
