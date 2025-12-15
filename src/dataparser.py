cbc5c4fd0ca69c223545f25b16d4f886a020a0d6
someotb <llyryk934@gmail.com>
2025-12-16 00:32:44 +0700

    refactor: change structure of project

--- src/dataparser.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-
-
-def lines_parser():
-    with open("../data/data.dat", "r", encoding="UTF-8") as file:
-        rand_num = randint(0, 99)
-        lines = file.readlines()
-        return lines[rand_num].strip()
